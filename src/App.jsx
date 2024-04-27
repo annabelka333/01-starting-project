f1106a1589512e2ee89930b828678b96c4c4445c
annabelka333 <139398794+annabelka333@users.noreply.github.com>
2024-04-28 00:18:56 +0300

    added dymanic button and conditional

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import CoreConcept from "./components/CoreConsept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
+  const [selectedTopic, setSelectedTopic] = useState("components");
 
   function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-    console.log(selectedTopic);
+    setSelectedTopic(selectedButton.toLowerCase());
+    console.log({ selectedTopic, EXAMPLES });
   }
 
   return (
@@ -33,12 +33,30 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect("components")}>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleSelect("components")}
+            >
               Components
             </TabButton>
-            <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleSelect("JSX")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleSelect("Props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleSelect("State")}
+            >
+              State
+            </TabButton>
           </menu>
 
           {!selectedTopic ? <p>Please selected a topic.</p> : null}
